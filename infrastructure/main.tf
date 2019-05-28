0831d53394d02568929f67eca6f3f22ac1a04c62
Alex Todorov <linked2u@gmail.com>
2019-05-28 23:40:01 +0300

    formatted .tf files

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,19 +1,19 @@
 # Define the provider that is going to be used
 
 provider "google" {
-credentials = "${file(/home/test/creds.json)}"
-project = "test-proj-241215"
-region = "${var.google_region}"
+  credentials = "${file(/home/test/creds.json)}"
+  project     = "test-proj-241215"
+  region      = "${var.google_region}"
 }
 
+# Define a random_pet provider that will generate names for server resources
 resource "random_pet" "srv_name" {
-length = 4
-prefix = "srv-"
+  length = 4
+  prefix = "srv-"
 }
 
+# Define a random_pet provider that will generate names for other than server resources
 resource "random_pet" "other_resource" {
-length = 10
-prefix = "other-"
+  length = 10
+  prefix = "other-"
 }
-
-
